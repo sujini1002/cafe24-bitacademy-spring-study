ed601f41e16b11ef2376179f1a635af0d429d43b
sujini <tgif20146@gmail.com>
2019-05-29 10:11:01 +0900

    2019.05.29 멀티 프로젝트 만들기

---
+++ spring-study-springex3/src/main/java/com/cafe24/springex/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.cafe24.springex.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.cafe24.springex.controller"})
+public class WebConfig {
+
+}
